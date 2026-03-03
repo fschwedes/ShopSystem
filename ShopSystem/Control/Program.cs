4c94e6720df1bf834d1be0f86151d49f5920e785
fschwedes <f.schwedes0@gmail.com>
2026-03-03 19:15:06 +0100

    Projektdateien hinzufügen.

---
+++ ShopSystem/Control/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopSystem.Control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
