5db15cf3713683898349bee5eed063d08820027a
fschwedes <f.schwedes0@gmail.com>
2026-03-04 15:59:50 +0100

    simple test authentication added

---
+++ ShopSystem/Control/AuthenticationControl.cs
@@ -0,0 +1,46 @@
+﻿using ShopSystem.Model;
+using ShopSystem.View;
+
+namespace ShopSystem.Control
+{
+    internal class AuthenticationControl
+    {
+        public void InitializeAuthentication()
+        {
+            const int validInput = 0;
+            const int wrongUserName = 1;
+            const int wrongPassword = 2;
+
+            AuthenticationView authenticationView = new AuthenticationView();
+            Account account = new Account("TestUsername", "TestPassword");
+
+            int wrongInput;
+            string usernameInput;
+            string passwordInput;
+
+            authenticationView.InitializeView(out usernameInput, out passwordInput);
+
+            do
+            {
+                if (usernameInput != account.Username)
+                {
+                    wrongInput = wrongUserName;
+                }
+                else if (passwordInput != account.Password)
+                {
+                    wrongInput = wrongPassword;
+                }
+                else
+                {
+                    wrongInput = validInput;
+                }
+
+                if (wrongInput != 0)
+                {
+                    authenticationView.InitializeView(wrongInput, out usernameInput, out passwordInput);
+                }
+            }
+            while (wrongInput != 0);
+        }
+    }
+}
