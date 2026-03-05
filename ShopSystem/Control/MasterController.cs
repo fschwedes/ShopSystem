b7b27712159527973849f8cff43dec331ec5b2ad
fschwedes <f.schwedes0@gmail.com>
2026-03-05 12:38:38 +0100

    class Item added list catalog of items added to MasterControl

---
+++ ShopSystem/Control/MasterController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using ShopSystem.Model;
+using ShopSystem.View;
+
+namespace ShopSystem.Control
+{
+    internal class MasterController
+    {
+        //private List<Account> _accounts;
+        private List<Item> _catalog;
+
+        public MasterController() 
+        {
+            init();
+        }
+
+
+        private void init()
+        {
+            //string path = "files/accounts.json";
+            //Directory.CreateDirectory("files");
+            //List<Account> accounts = new List<Account>() { new Account("Test", "123")
+            //    , new Account("Test2", "321")};
+            //string json = JsonSerializer.Serialize(accounts);
+            //File.WriteAllText(path, json);
+
+            //string pathItems = "files/items.json";
+            //List<Item> catalog = new List<Item>() { new Item("Test", 13.5)
+            //    , new Item("Test2", 20, "This is a test")};
+            //string jsonItems = JsonSerializer.Serialize(catalog);
+            //File.WriteAllText(pathItems, jsonItems);
+
+            string pathItems = "files/items.json";
+            string outputItems = File.ReadAllText(pathItems);
+            _catalog = JsonSerializer.Deserialize<List<Item>>(outputItems) 
+                ?? new List<Item>();
+
+            //string pathAccounts = "files/accounts.json";
+            //string outputAccounts = File.ReadAllText(pathAccounts);
+            //_accounts = JsonSerializer.Deserialize<List<Account>>(outputAccounts)
+            //    ?? new List<Account>();
+        }
+
+        //public List<Account> Accounts()
+        //{ return _accounts; }
+        public List<Item> Catalog()
+        { return _catalog; }
+    }
+}
